6b2fbb53235a193a31b3f8b78b71d6a396f21fcb
A-mitsunaga <a56st70410@icloud.com>
2021-04-29 16:22:20 +0000

    [Add5]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,25 @@ class BooksController < ApplicationController
 def new
  @book = Book.new
  end
+
 def create
 book = Book.new(book_params)
 book.save
 redirect_to book_path(book.id)
 
-flash[:notice] = "Book was successfully created."
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
 
 def index
@@ -16,6 +29,9 @@ end
 
 def show
   @book = Book.find(params[:id])
+  flash[:notice] = "Book was successfully created."
+
+
 end
 
 def edit
@@ -25,7 +41,7 @@ end
 def update
  book = Book.find(params[:id])
  book.update(book_params)
- redirect_to book_path(book.id)
+ redirect_to book_path(book)
 
  flash[:notice] = "Book was successfully updated."
 end
@@ -34,6 +50,8 @@ def destroy
  book = Book.find(params[:id])
  book.destroy
  redirect_to books_path
+
+ flash[:notice] = "Book was successfully destroyed."
 end
 
 
