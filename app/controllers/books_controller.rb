f0b4840e564adf7ee12443e4418f3aa8b04590ba
A-mitsunaga <a56st70410@icloud.com>
2021-04-30 02:19:06 +0000

    [Add6]task

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,62 +1,46 @@
 class BooksController < ApplicationController
-def new
- @book = Book.new
+
+ def new
+  @book = Book.new
  end
 
-def create
-book = Book.new(book_params)
-book.save
-redirect_to book_path(book.id)
+ def create
+  book = Book.new(book_params)
+  book.save
 
+  redirect_to book_path(id: book)
+ end
 
+ def index
+   @books = Book.all
+ end
 
-
-
-
-
-
-
-
-
-
-
-
-end
-
-def index
-  @books = Book.all
-end
-
-def show
+ def show
   @book = Book.find(params[:id])
-  flash[:notice] = "Book was successfully created."
+ end
+
+ def edit
+  @book = Book.find(params[:id])
+ end
+
+ def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book)
+
+ end
+
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
+
+ end
 
 
-end
+ private
+  def book_params
+   params.require(:book).permit(:title,:body)
+  end
 
-def edit
-@book = Book.find(params[:id])
-end
-
-def update
- book = Book.find(params[:id])
- book.update(book_params)
- redirect_to book_path(book)
-
- flash[:notice] = "Book was successfully updated."
-end
-
-def destroy
- book = Book.find(params[:id])
- book.destroy
- redirect_to books_path
-
- flash[:notice] = "Book was successfully destroyed."
-end
-
-
-private
-def book_params
-params.permit(:title,:body)
-end
 end
